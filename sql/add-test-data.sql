56752fb4c3c658435e9dd97392713767e1ebe45b
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-04-04 22:33:45 +0300

    viikko 2

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,7 +1,5 @@
-INSERT INTO Auto (id, rekkari, merkki, malli) VALUES (1, ABC-123, Lada, 100);
-
-INSERT INTO Kuljettaja (id, etunimi, sukunimi) VALUES (1, Arto, Vihavainen);
-
-INSERT INTO Ajovuoro (id, kuljettaja_id, auto_id, km, aika) VALUES (1, 1, 1, 11.2, 0.40);
-
-INSERT INTO Kyyti (id, ajovuoro_id, hinta, km, aika) VALUES (1, 1, 25.2, 9.63, 0.15);
+INSERT INTO Auto (rekkari, asemapaikka, merkki, malli) VALUES ('ABC-123', 'Helsinki', 'Lada', '100');
+INSERT INTO Kuljettaja (etunimi, sukunimi) VALUES ('Arto', 'Vihavainen');
+INSERT INTO Ajovuoro (kuljettaja_id, auto_id, km, aika) VALUES (1, 1, 11.2, 0.40);
+INSERT INTO Kyyti (ajovuoro_id, hinta, km, aika) VALUES (1, 25.2, 9.63, 0.15);
+INSERT INTO Kayttaja (tunnus, salasana) VALUES ('a', 'b');
