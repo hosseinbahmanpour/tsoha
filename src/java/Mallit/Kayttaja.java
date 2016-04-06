38f06a93cb3ac00e25ad99348be1bd5da7095b5d
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-04-06 17:20:22 +0300

    bugit korjattu

--- src/java/Mallit/Kayttaja.java
+++ src/java/Mallit/Kayttaja.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.naming.NamingException;
 
 public class Kayttaja {
@@ -17,6 +19,9 @@ public class Kayttaja {
         this.id = id;
         this.tunnus = tunnus;
         this.salasana = salasana;
+    }
+    
+    public Kayttaja() {
     }
 
     public int getId() {
@@ -43,11 +48,37 @@ public class Kayttaja {
         this.salasana = salasana;
     }
 
-    public static void paulautaKaikki() throws SQLException, NamingException {
-        Tietokanta t = new Tietokanta();        
+    public static List<Kayttaja> getKayttajat() throws NamingException, SQLException {
+        String sql = "SELECT id, tunnus, salasana FROM kayttaja";
+        Tietokanta t = new Tietokanta();
         Connection yhteys = t.getYhteys();
-        String sql = "SELECT id, tunnus, salasana from kayttajat";
         PreparedStatement kysely = yhteys.prepareStatement(sql);
-        ResultSet rs = kysely.executeQuery();
+        ResultSet tulokset = kysely.executeQuery();
+
+        ArrayList<Kayttaja> kayttajat = new ArrayList<Kayttaja>();
+        while (tulokset.next()) {
+            //Luodaan tuloksia vastaava olio ja palautetaan olio:
+            Kayttaja k = new Kayttaja();
+            k.setId(tulokset.getInt("id"));
+            k.setTunnus(tulokset.getString("tunnus"));
+            k.setSalasana(tulokset.getString("salasana"));
+
+            kayttajat.add(k);
+        }
+        //Suljetaan kaikki resutuloksetsit:
+        try {
+            tulokset.close();
+        } catch (Exception e) {
+        }
+        try {
+            kysely.close();
+        } catch (Exception e) {
+        }
+        try {
+            yhteys.close();
+        } catch (Exception e) {
+        }
+
+        return kayttajat;
     }
 }
