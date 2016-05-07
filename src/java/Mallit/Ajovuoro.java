0746981bdbfc6de6b09f977709e038b7d96e1328
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-05-07 16:11:02 +0300

    Final

--- src/java/Mallit/Ajovuoro.java
+++ src/java/Mallit/Ajovuoro.java
@@ -1,5 +1,12 @@
 package Mallit;
 
+import Tietokanta.Tietokanta;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+
 public class Ajovuoro {
 
     private int id;
@@ -28,5 +35,32 @@ public class Ajovuoro {
 
     public void setAutoId(int autoId) {
         this.autoId = autoId;
+    }
+
+    public static int etsi(int kuskiId, int autoId) throws NamingException, SQLException {
+        String sql = "SELECT id FROM Ajovuoro WHERE kuljettaja_id=?, auto_id=?;";
+        Tietokanta t = new Tietokanta();
+        Connection yhteys = t.getYhteys();
+        PreparedStatement kysely = yhteys.prepareStatement(sql);
+        kysely.setInt(1, kuskiId);
+        kysely.setInt(2, autoId);
+        ResultSet tulokset = kysely.executeQuery();
+        tulokset.next();
+        int id = Integer.parseInt(tulokset.getString("id"));
+        t.sulje(tulokset, kysely);
+        return id;
+    }
+
+    public void lisaaKantaan() throws NamingException, SQLException {
+        String sql = "INSERT INTO Ajovuoro(kuljettaja_id, auto_id) VALUES(?,?) RETURNING id;";
+        Tietokanta t = new Tietokanta();
+        Connection yhteys = t.getYhteys();
+        PreparedStatement kysely = yhteys.prepareStatement(sql);
+        kysely.setInt(1, this.getKuljettajaId());
+        kysely.setInt(2, this.getAutoId());
+        ResultSet tulokset = kysely.executeQuery();
+        tulokset.next();
+        this.id = tulokset.getInt(1);
+        t.sulje(tulokset, kysely);
     }
 }
